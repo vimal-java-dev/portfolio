f33e5e2c98f0402e72439f75fc62f069d1ec6205
vimal-java-dev <vimal929@gmail.com>
2026-03-16 08:26:21 +0530

    Footer improved with 3 column section
    Signed-off-by: vimal-java-dev <vimal929@gmail.com>

--- js/open-modal-btn.js
+++ js/open-modal-btn.js
@@ -4,18 +4,24 @@ const btn = document.querySelector(".open-modal-btn");
 const close = document.querySelector(".close");
 
 // Open modal on button click
-btn.addEventListener("click", () => {
+if (btn && modal) {
+  btn.addEventListener("click", () => {
     modal.classList.add("show");
-});
+  });
+}
 
 // Close modal on close button click
-close.addEventListener("click", () => {
+if (close && modal) {
+  close.addEventListener("click", () => {
     modal.classList.remove("show");
-});
+  });
+}
 
 // Close modal on clicking outside the modal content
-window.addEventListener("click", (e) => {
+if (modal) {
+  window.addEventListener("click", (e) => {
     if (e.target === modal) {
-        modal.classList.remove("show");
+      modal.classList.remove("show");
     }
-});
+  });
+}
